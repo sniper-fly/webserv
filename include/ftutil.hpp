b65c3a10d601b14d37610db55ee343a1e8cddd54
rnakai <rnakai@student.42tokyo.jp>
2022-04-11 15:07:50 +0900

    余計なft::を削除

--- include/ftutil.hpp
+++ include/ftutil.hpp
@@ -1,10 +1,12 @@
 #ifndef FTUTIL_HPP
 #define FTUTIL_HPP
 
+#include <string>
+
 namespace ft
 {
-  char* ft::strlwr(char* str);
-  int   ft::stricmp(const char* s1, const char* s2);
+  char* strlwr(char* str);
+  int   stricmp(const char* s1, const char* s2);
   void  copyFile(std::string from, std::string to);
 } // namespace ft
 #endif
